92997450174dd3159ae81924378849257789b444
tangqifeng1111@163.com <tangqifeng1111@163.com>
2020-02-11 15:15:56 +0800

    remove the lombok

--- zero-web/src/main/java/com/firefly/zero/web/model/WeChatAccessToken.java
+++ zero-web/src/main/java/com/firefly/zero/web/model/WeChatAccessToken.java
@@ -6,24 +6,95 @@
  */
 package com.firefly.zero.web.model;
 
-import lombok.*;
-
 import java.util.Date;
 
-@Getter(value = AccessLevel.PUBLIC)
-@Setter(value = AccessLevel.PUBLIC)
-@NoArgsConstructor
-@ToString
-@EqualsAndHashCode
-@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
 public class WeChatAccessToken {
 
-    @NonNull
     private String uuid;
-
-    @NonNull
     private String accessToken;
     private Date createTime;
     private Date lastModifiedTime;
 
+    /**
+     * Empty constructor of {@link WeChatAccessToken}.
+     */
+    public WeChatAccessToken() {
+        // empty
+    }
+
+    /**
+     * Constructor of {@link WeChatAccessToken}, with uuid and access token specified.
+     *
+     * @param uuid
+     * @param accessToken
+     */
+    public WeChatAccessToken(final String uuid, final String accessToken) {
+        this.uuid = uuid;
+        this.accessToken = accessToken;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(final String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(final String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(final Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastModifiedTime() {
+        return lastModifiedTime;
+    }
+
+    public void setLastModifiedTime(final Date lastModifiedTime) {
+        this.lastModifiedTime = lastModifiedTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WeChatAccessToken that = (WeChatAccessToken) o;
+
+        if (uuid != null ? !uuid.equals(that.uuid) : that.uuid != null) return false;
+        if (accessToken != null ? !accessToken.equals(that.accessToken) : that.accessToken != null) return false;
+        if (createTime != null ? !createTime.equals(that.createTime) : that.createTime != null) return false;
+        return lastModifiedTime != null ? lastModifiedTime
+                .equals(that.lastModifiedTime) : that.lastModifiedTime == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = uuid != null ? uuid.hashCode() : 0;
+        result = 31 * result + (accessToken != null ? accessToken.hashCode() : 0);
+        result = 31 * result + (createTime != null ? createTime.hashCode() : 0);
+        result = 31 * result + (lastModifiedTime != null ? lastModifiedTime.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "WeChatAccessToken{" +
+                "uuid='" + uuid + '\'' +
+                ", accessToken='" + accessToken + '\'' +
+                ", createTime=" + createTime +
+                ", lastModifiedTime=" + lastModifiedTime +
+                '}';
+    }
 }
--- zero-web/src/main/java/com/firefly/zero/web/model/WeChatGlobalConfig.java
+++ zero-web/src/main/java/com/firefly/zero/web/model/WeChatGlobalConfig.java
@@ -6,19 +6,11 @@
  */
 package com.firefly.zero.web.model;
 
-import lombok.*;
-
 import java.util.Date;
 
 /**
  * The container, which contains the global configuration for <code>WeChat</code>
  */
-@Getter(value = AccessLevel.PUBLIC)
-@Setter(value = AccessLevel.PUBLIC)
-@NoArgsConstructor
-@ToString
-@EqualsAndHashCode
-@Builder
 public class WeChatGlobalConfig {
 
     private int id;
@@ -28,5 +20,109 @@ public class WeChatGlobalConfig {
     private String token;
     private Date createTime;
 
+    /**
+     * Empty constructor of {@link WeChatGlobalConfig}.
+     */
+    public WeChatGlobalConfig() {
+        // empty
+    }
+
+    /**
+     * Constructor of {@link WeChatGlobalConfig}, with app id, secret, encoding AES key and token specified.
+     */
+    public WeChatGlobalConfig(final String appId, final String appSecret, final String encodingAESKey,
+            final String token) {
+        this.appId = appId;
+        this.appSecret = appSecret;
+        this.encodingAESKey = encodingAESKey;
+        this.token = token;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public void setAppSecret(String appSecret) {
+        this.appSecret = appSecret;
+    }
+
+    public String getEncodingAESKey() {
+        return encodingAESKey;
+    }
+
+    public void setEncodingAESKey(String encodingAESKey) {
+        this.encodingAESKey = encodingAESKey;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WeChatGlobalConfig that = (WeChatGlobalConfig) o;
+
+        if (id != that.id) return false;
+        if (appId != null ? !appId.equals(that.appId) : that.appId != null) return false;
+        if (appSecret != null ? !appSecret.equals(that.appSecret) : that.appSecret != null) return false;
+        if (encodingAESKey != null ? !encodingAESKey.equals(that.encodingAESKey) : that.encodingAESKey != null)
+            return false;
+        if (token != null ? !token.equals(that.token) : that.token != null) return false;
+        return createTime != null ? createTime.equals(that.createTime) : that.createTime == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (appId != null ? appId.hashCode() : 0);
+        result = 31 * result + (appSecret != null ? appSecret.hashCode() : 0);
+        result = 31 * result + (encodingAESKey != null ? encodingAESKey.hashCode() : 0);
+        result = 31 * result + (token != null ? token.hashCode() : 0);
+        result = 31 * result + (createTime != null ? createTime.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "WeChatGlobalConfig{" +
+                "id=" + id +
+                ", appId='" + appId + '\'' +
+                ", appSecret='" + appSecret + '\'' +
+                ", encodingAESKey='" + encodingAESKey + '\'' +
+                ", token='" + token + '\'' +
+                ", createTime=" + createTime +
+                '}';
+    }
 }
 
