5f67fa29a5594e20c92dfa186e5280826206f64d
DorseyGo <tangqifeng1111@163.com>
2020-02-26 16:01:05 +0800

    add remember me function

--- zero-web/src/main/java/com/firefly/zero/web/config/WebSecurityConfig.java
+++ zero-web/src/main/java/com/firefly/zero/web/config/WebSecurityConfig.java
@@ -8,12 +8,18 @@ package com.firefly.zero.web.config;
 
 import com.firefly.zero.web.auth.ZeroUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +27,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private ZeroUserDetailsService userDetailsService;
+
+    @Resource
+    private DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -37,12 +46,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().csrf().disable();
 
-//        http.rememberMe().tokenRepository(persistentTokenRepository())
-//                .tokenValiditySeconds(3600)
-//                .userDetailsService(userDetailsService);
+        http.rememberMe().tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService);
 
         http.headers().frameOptions().sameOrigin();
         http.logout().logoutSuccessUrl("/login").permitAll();
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        final JdbcTokenRepositoryImpl repository = new JdbcTokenRepositoryImpl();
+        repository.setDataSource(dataSource);
+
+        return repository;
     }
 
 }
