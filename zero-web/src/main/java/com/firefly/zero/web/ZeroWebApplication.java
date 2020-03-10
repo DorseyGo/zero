0d6887449f8e4d9eb522e70c0c02fc5f3b64ff66
tangqifeng1111@163.com <tangqifeng1111@163.com>
2020-03-10 22:12:47 +0800

    add zero-mini

--- zero-web/src/main/java/com/firefly/zero/web/ZeroWebApplication.java
+++ zero-web/src/main/java/com/firefly/zero/web/ZeroWebApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 @MapperScan(basePackages = {"com.firefly.zero.web.dao"})
 @SpringBootApplication
 public class ZeroWebApplication {
--- zero-web/src/main/java/com/firefly/zero/web/service/impl/GroupServiceImpl.java
+++ zero-web/src/main/java/com/firefly/zero/web/service/impl/GroupServiceImpl.java
@@ -29,6 +29,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Transactional
 @Service("groupService")
